d6dca1335591c25a3c7383e166b8b08640c5b245
Leite <pedro.leite@fujitsu.com>
2022-07-07 16:32:43 +0100

    new update

--- background.js
+++ background.js
@@ -1,4 +1,50 @@
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    let url = tabs[0].url;
-    // use `url` here inside the callback because it's asynchronous!
-});
+// //this will log in the devTools console log
+// let url = "";
+// let popUpInfo = "";
+
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+// console.log('message :', message);
+
+// //   chrome.runtime.sendMessage({ data: 'info' }, function (response) {
+// //     console.log('Response: ' + response)
+// //   });
+
+// });
+
+// chrome.runtime.sendMessage({data:popUpInfo},function(response){
+	
+// });
+
+// chrome.tabs.onActivated.addListener((tab) => {
+//   chrome.tabs.get(tab.tabId, (current_tab_info) => {
+//     if (tab.tabId) {
+//       chrome.tabs.executeScript(null, { file: "./foreground.js" }, () => {
+//         console.log("Injected!");
+//       });
+//     }
+//   });
+// });
+
+// // chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+// //     url = tabs[0].url;
+// //     // use `url` here inside the callback because it's asynchronous!
+// // });
+
+// // chrome.browserAction.onClicked.addListener(function(tab){
+
+// //     chrome.tabs.create({'url': chrome.extension.getURL('popup.html')},function(tab) {
+// //         chrome.tabs.sendMessage(id, data, function (url) {
+// //             console.log(url);
+// //         });
+// //    });
+// //   })
+
+    chrome.tabs.onActivated.addListener((tab) => {
+          chrome.tabs.get(tab.tabId, (current_tab_info) => {
+            if (tab.tabId) {
+                    chrome.tabs.executeScript(null, { file: "./foreground.js" }, () => {
+                                console.log("Foreground script Injected!");
+                        });
+            }
+          });
+        });
---
+++ popup.js
@@ -0,0 +1,25 @@
+// // chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+// //     console.log(msg);
+// //   });
+
+// //this sends info to background as soon as the popup is open/clicked
+// var infoData = 'HANDSHAKE? DONT KNOW!'
+
+// chrome.runtime.sendMessage({data:infoData},function(response){
+	
+// });
+
+// // chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+// //     document.getElementById("first").innerHTML = 'addListener';
+// //     // document.getElementById("second").innerHTML = response;
+    
+// // });
+
+async function fetchData() {
+    const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
+    const record=await res.json();
+    // document.getElementById("first").innerHTML = record.data[0].areaName;
+    $("#first").text(record.data[0].areaName);
+    $("h3").append('ADD');
+}
+fetchData();
